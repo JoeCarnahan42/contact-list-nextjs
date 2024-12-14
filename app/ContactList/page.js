5273607e28a42d90719422bad766ce04bd51cf7e
JoeCarnahan42 <carnahanjoseph@yahoo.com>
2024-12-13 21:32:20 -0600

    Change state control component for list. Add TODO's

--- app/ContactList/page.js
+++ app/ContactList/page.js
@@ -1,24 +1,23 @@
 'use client'
 import 'bootstrap/dist/css/bootstrap.css'
-import Data from "../Data/page"
-import { useState } from "react"
 import Image from "next/image"
 
-export default function ContactList() {
-  console.log(Data)
-  const [contacts, _] = useState(Data.all())
+export default function ContactList({ prop }) {
+  console.log(prop)
+  // TODO : 
+  const contacts = prop
 
   return (
     <div>
       {contacts.map((c) => (
-        <ul className=' list-group list-group-horizontal justify-content-center' key={c.id}>
-          <li className="border-0 list-group-item">
-            <Image width={50} height={50} alt="unc" src={c.image_url} />
+        <ul className='list-group list-group-horizontal justify-content-center' key={c.id}>
+          <li className="border-0 list-group-item bg-secondary">
+            <Image width={40} height={40} alt="unc" src={c.image_url} />
           </li>
-          <li className='border-0 list-group-item'>{c.name}</li>
-          <li className='border-0 list-group-item'>{c.email}</li>
-          <li className='border-0 list-group-item'>{c.phone_number}</li>
-          <li className='border-0 list-group-item'><a className='link'>Edit</a></li>
+          <li className='border-0 list-group-item bg-secondary'>{c.name}</li>
+          <li className='border-0 list-group-item bg-secondary'>{c.email}</li>
+          <li className='border-0 list-group-item bg-secondary'>{c.phone_number}</li>
+          <li className='border-0 list-group-item bg-secondary'><a className='link'>Edit</a></li>
         </ul>
       ))}
     </div>
--- app/contacts/page.js
+++ app/contacts/page.js
@@ -1,8 +1,25 @@
-import Link from "next/link"
-import ContactList from "../ContactList/page"
+'use client';
+import Link from "next/link";
+import ContactList from "../ContactList/page";
+import { useState } from 'react'
+import Data from "../Data/page";
 
 
 export default function Contacts() {
+  const [search, setSearch] = useState(Data.all())
+  const searchCon = (e) => {
+    console.log(e.target.value)
+    // TODO : normalize input
+    if (e.target.value === '') {
+      setSearch(Data.all())
+    } else {
+      console.log(search)
+      const newList = Data.all().filter((c) => {
+        return c.name.includes(e.target.value)
+      })
+      setSearch(newList)
+    }
+  };
   return (
     <main>
       <div className='text-center'>
@@ -10,9 +27,11 @@ export default function Contacts() {
         <Link className='btn btn-primary' href='/contacts/new'>Add Contact</Link>
       </div>
       <br />
-      <div>
-        <ContactList />
+      <div >
+        <input id="con-search" onChange={searchCon} className="input-group-text m-auto" placeholder="Search Contacts" type="text"></input>
+        <br />
+        <ContactList prop={search} />
       </div>
   </main>
-  )
-}
+  );
+};
